f14abaa4ad87bb1e6f444959bfb3367d0f3ae320
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-25 14:35:35 +0200

    add styles for profile

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,17 +1,33 @@
 import styled from 'styled-components';
 
 export const ProfileCard = styled.div`
-  display: flex;
-  justify-content: center;
   margin: 0 auto;
   margin-top: 20px;
-  width: 400px;
-  height: 500px;
+  /* padding: 30px; */
+  width: 300px;
+  height: 400px;
   border: 2px solid grey;
   border-radius: 8px;
 `;
 
 export const ProfileAvatar = styled.img`
-  border-radius: 50%;
+  display: block;
+  margin: 0 auto;
   background-color: red;
+  text-align: center;
+  border: 2px solid black;
+  border-radius: 50%;
+`;
+
+export const ProfileName = styled.div`
+  margin-top: 20px;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const ProfileDescription = styled.div`
+  margin-top: 10px;
+  color: grey;
+  text-align: center;
 `;
