9f0562b6ce4cc93f26195d118a0f088fbb3a3e88
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-25 15:37:55 +0200

    add reset styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -16,8 +16,34 @@ code {
     monospace;
 }
 
-ul {
-  list-style: none;
-  padding: 0;
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
 }
+a {
+  text-decoration: none;
+}
+
+ul,
+ol {
+  list-style: none;
+  margin: 0;
+  padding-left: 0;
+}
+
+button {
+  cursor: pointer;
+}
+
+img {
+  display: block;
+  
+  height: auto;
+}
+
 `;
