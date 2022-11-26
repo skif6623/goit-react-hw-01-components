132d23d56400827b40f3c92ba47b8be9675b731c
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-26 17:34:47 +0200

    add transactions file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import user from 'user';
 import data from 'data';
 import friends from 'friends';
+import transactions from 'transactions';
 import { GlobalStyle } from './GlobalStyle';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
